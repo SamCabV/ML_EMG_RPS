e293e2e4386591b9a3c35b00825da9d0ad7a8ec3
SamCabV <scabreravalencia@olin.edu>
2023-12-01 20:12:32 -0500

    Final_offline_push

--- modified_test2.py
+++ modified_test2.py
@@ -12,10 +12,10 @@ from sklearn.preprocessing import label_binarize
 import seaborn as sns
 
 # Selecting the specified columns
-#feature_table = pd.read_csv("new_data - Sheet1.csv")
-#labels = pd.read_csv("final_dataset_labels - Sheet1.csv")
-feature_table = pd.read_csv("no_aug_dataset - Sheet1.csv")
-labels = pd.read_csv("no_aug_labels - Sheet1.csv")
+feature_table = pd.read_csv("new_data - Sheet1.csv")
+labels = pd.read_csv("final_dataset_labels - Sheet1.csv")
+#feature_table = pd.read_csv("no_aug_dataset - Sheet1.csv")
+#labels = pd.read_csv("no_aug_labels - Sheet1.csv")
 
 classes = ['Rock', 'Paper', 'Scissors', 'Rest']
 selected_columns = [0,1,2,3,4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14,15,16, 17, 18, 19]
@@ -27,25 +27,32 @@ features_tensor = torch.tensor(features.values, dtype=torch.float32)
 labels_tensor = torch.tensor(labels.values, dtype=torch.long)
 
 # Splitting the dataset into training and testing sets
-#X_train, X_test, y_train, y_test = train_test_split(
-#    features_tensor, labels_tensor, train_size=5770, test_size=2000, stratify=labels_tensor, random_state=42
-#)
 X_train, X_test, y_train, y_test = train_test_split(
-    features_tensor, labels_tensor, train_size=3389, test_size=498, stratify=labels_tensor, random_state=42
+    features_tensor, labels_tensor, train_size=5770, test_size=2000, stratify=labels_tensor, random_state=42
 )
+#X_train, X_test, y_train, y_test = train_test_split(
+#    features_tensor, labels_tensor, train_size=3389, test_size=498, stratify=labels_tensor, random_state=42
+#)
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Classifiers to be used
 classifiers = {
-    "AdaBoostClassifier": ensemble.AdaBoostClassifier(),
-    "BaggingClassifier": ensemble.BaggingClassifier(),
-    "ExtraTreesClassifier": ensemble.ExtraTreesClassifier(),
-    "GradientBoostingClassifier": ensemble.GradientBoostingClassifier(),
-    "HistGradientBoostingClassifier": ensemble.HistGradientBoostingClassifier(),
+   # "AdaBoostClassifier": ensemble.AdaBoostClassifier(),
+   # "BaggingClassifier": ensemble.BaggingClassifier(),
+   # "ExtraTreesClassifier": ensemble.ExtraTreesClassifier(),
+   # "GradientBoostingClassifier": ensemble.GradientBoostingClassifier(),
+   # "HistGradientBoostingClassifier": ensemble.HistGradientBoostingClassifier(),
     "RandomForestClassifier (Optimized) n=491, Max_leaf = 807": ensemble.RandomForestClassifier(n_estimators=491, max_leaf_nodes=807, random_state=42),
     "RandomForestClassifier n=200, Max_leaf = 400": ensemble.RandomForestClassifier(n_estimators=200, max_leaf_nodes=400 , random_state=42),
+    "RandomForestClassifier n=100, Max_leaf = 50": ensemble.RandomForestClassifier(max_leaf_nodes=50, random_state=42),    
+    "RandomForestClassifier  n=100, Max_leaf = 100": ensemble.RandomForestClassifier(max_leaf_nodes=100, random_state=42),    
+    "RandomForestClassifier n=100, Max_leaf = 200": ensemble.RandomForestClassifier(max_leaf_nodes=200, random_state=42),    
+    "RandomForestClassifier (default) n=100, Max_leaf = None": ensemble.RandomForestClassifier(random_state=42),    
 
-    "RandomForestClassifier (default) n=100, Max_leaf = None": ensemble.RandomForestClassifier(random_state=42)
+    "RandomForestClassifier n=50, Max_leaf = None": ensemble.RandomForestClassifier(n_estimators=50, random_state=42),
+    "RandomForestClassifier n=10, Max_leaf = None": ensemble.RandomForestClassifier(n_estimators=10, random_state=42),
+    "RandomForestClassifier n=150, Max_leaf = None": ensemble.RandomForestClassifier(n_estimators=150, random_state=42)
+
 }
 
 # For storing f1 scores
